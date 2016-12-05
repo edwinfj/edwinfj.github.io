74ec1dca5e20d7824b79827f87c53f85a427e5fd
Jing Fang <jing.fang.zju@gmail.com>
2016-12-05 23:21:48 +0800

    listed all open courses I took. used javascript to replace some repeat html

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+	// open all hyperlinks in the new tab, except links in the banner and blog
+	$('#intro a, #olcourse a, .projectitem a').attr('target', '_blank');
+	// asign attributes to olcourseitem children
+	$('.olcourseitem > div').attr('class', 'flexcontainer flexbottom');
+	$('.olcourseitem h2').attr('class', 'col7-l col12-s');
+	$('.olcourseitem a').attr('title', 'course link');
+	$('.difficulty').attr('class', 'col2-l col12-s difficulty');
+	$('.recommend').attr('class', 'col3-l col12-s recommend');
+
+	// set header contents
+	$('header').html('<h1>Welcome to my Novoland</h1>' +
+		'<ul id="bannerlist" class="col12-s col6-l clearfloat nopaddinglist">' +
+		'<a href="index.html" class="col4-s col4-l"><li>Home</li></a>' +
+		'<a href="demo.html" class="col4-s col4-l"><li>Demo</li></a>' +
+		'<a href="blog.html" class="col4-s col4-l"><li>Blog</li></a></ul>');
+
+	// set repo link images
+	$('.repolink a').html('<img src="images/github.svg" alt="repository" title="repository" class="svg_icon svg_icon_github">')
+	// assign attributes to demo project items
+	$('.demoLink').attr('title', 'demo');
+	$('.projectitem > div').attr('class', 'flexcontainer flexbottom');
+	$('.projectitem h2').attr('class', 'col4-l col12-s');
+	$('.repolink').attr('class', 'repolink col4-l col6-s');
+	$('.titlenote').attr('class', 'titlenote col4-l col6-s');
+
     // convert difficulty level to corresponding visual effect
     var level = ['beginner', 'intermediate', 'advanced'];
     level.forEach(function(item, index) {
-        $(".difficulty:contains(" + item +")").html(item + " " + "&#x25A0;".repeat(index) + "&#x25A1;".repeat(3 - index));
+        $(".difficulty:contains(" + item +")").html(item + " " + "&#x25A0;".repeat(1+index) + "&#x25A1;".repeat(2 - index));
     });
     // convert recommend level to corresponding visual effect
     var recommend = [1, 2, 3, 4, 5];
